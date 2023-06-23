7e0cfa460981412a9c5261508cc181bda90bde8a
LeeGiTaek <you@example.com>
2023-06-22 22:25:58 -0700

    add loading, error

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,24 +1,33 @@
 import React, { useState, useEffect } from "react";
 
 export default function Products() {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState();
+
     const [count, setCount] = useState(0);
     const [products, setProducts] = useState([]);
     const [checked, setChecked] = useState(false);
     const handleChange = () => setChecked((prev) => !prev);
 
     useEffect(() => {
+        setLoading(true);
+        setError(undefined);
         fetch(`data/${checked ? 'sale_': ''}products.json`)
             .then((res) => res.json())
             .then((data) => {
                 console.log('🔥 fetched the most recent data from network!');
                 setProducts(data);
-            });
+            }).catch(e=>setError('🙃Error!'))
+            .finally(()=> setLoading(false));
 
         return () => {
             console.log('🧶 Clean!');
         };
     }, [checked]);
 
+    if(loading) return <p>Loading...</p>;
+    if(error) return <p>{error}</p>;
+    
     return (
         <>
             <input type='checkbox' value={checked} onChange={handleChange} />
